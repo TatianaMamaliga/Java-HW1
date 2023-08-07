9bd836a8bbd75caaaa9c4a0bb751c2b7c0ca60fc
TatianaMamaliga <125645241+TatianaMamaliga@users.noreply.github.com>
2023-08-07 21:04:15 +0300

    session4

---
+++ src/session4/Practice/JavaStatements.java
@@ -0,0 +1,44 @@
+package session4.Practice;
+
+public class JavaStatements {
+
+    public static void main(String[] args) {
+        int currentYear = 2023;
+        int birthday = 1990;
+        int age = currentYear - birthday;
+        System.out.println("Age is: " + age);
+
+        if(age >= 18){
+            System.out.println("Eligible to vote");
+        }
+
+        int number = 15;
+        if (number > 0){
+            System.out.println("Number is positive.");
+        } else if (number < 0) {
+            System.out.println("Number is negative.");
+        } else {
+            System.out.println("Number is zero.");
+        }
+
+        printName();
+    }
+
+    public static void printName(){
+        String name = "Ioana";
+        String city = "Cluj";
+        String sex = "F";
+        int age = 20;
+
+        if(city.equals("Cluj")){
+            if(sex.equals("F")){
+                if(age == 20){
+                    System.out.println(name);
+                }
+            }
+        }
+        if(city.equals("Cluj") && sex.equals("F") && age == 20){
+            System.out.println(name);
+        }
+    }
+}
